4c92d6f64aebac85101c4080124ab5eb9007071c
idoab <ido.abudi118@gmail.com>
2025-06-07 23:40:33 +0300

    add User.java

--- app/src/main/java/com/example/triviavirsion2/Auth.java
+++ app/src/main/java/com/example/triviavirsion2/Auth.java
@@ -12,20 +12,15 @@ public class Auth {
         this.email = email;
         this.password = password;
     }
-
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getEmail() {
         return email;
     }
-
     public String getPassword() {
         return password;
     }
